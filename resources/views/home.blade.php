07121b1d368a14f88963b9cd0e178e68faa6c27e
takabo <thousandbeach.td@gmail.com>
2018-11-21 22:15:08 +0900

    npm install した

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <task-component></task-component>
+                    <example-component></example-component>
                 </div>
             </div>
         </div>
